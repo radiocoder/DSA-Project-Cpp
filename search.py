9af621e1a406750451b0177a270d5db7eb303873
Hajrah <hvirk.bscs20seecs@seecs.edu.pk>
2021-12-31 13:47:36 +0500

    Documented the code thoroughly.

--- search.py
+++ search.py
@@ -4,31 +4,34 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
-
+# function to perform the search recursively
 def search_inverted(all_doc_ids, query_word_ids, last):
     if (last == -1): # base condition
         return all_doc_ids
     word_id = query_word_ids[last]
-    #print(word_id)
     doc_id_list = inverted_index[str(word_id)].keys()
     docs = set(doc_id_list)
     all_doc_ids = set(all_doc_ids) | docs # take union with all doc ids
     return docs & search_inverted(all_doc_ids, query_word_ids, last-1) # recursively take intersections
 
 
+# load the inverted index, lexicon and document index in memory
 inverted_index = json.loads(Path("inverted_index.json").read_text())
 lexicon = json.loads(Path("lexicon.json").read_text())
-del lexicon['']
 doc_index = json.loads(Path("doc_index.json").read_text())
 
-stemmer = SnowballStemmer('english')
-stop_words = set(stopwords.words('english'))
+
+stemmer = SnowballStemmer('english') # construct a stemmer to stem the query
+stop_words = set(stopwords.words('english')) # to remove stopwords from query
 stop_words.add('the')
 
 while True:
-    query = input("Enter query: ")
+    query = input("Enter query: ") # take query from user
 
-    query_words = query.split()
+    query_words = query.split() # tokenize the query words
+    # santize the query:
+    # remove punctuations, stopwords
+    # stem the query words
     query_words = [w.lower() for w in query_words]
     query_words = [w for w in query_words if w not in stop_words]
     table = str.maketrans('', '', string.punctuation)
@@ -36,6 +39,7 @@ while True:
     query_words = [word for word in stripped if word.isalpha()]
     query_words = [stemmer.stem(w) for w in query_words]
 
+    # get the word ids of the words from lexicon
     query_word_ids = []
     for word in query_words:
         query_word_ids.append(lexicon.get(word, ''))
@@ -46,22 +50,31 @@ while True:
     else:
         empty_set = {}
         docs = search_inverted(empty_set, query_word_ids, len(query_word_ids) - 1)
+        while len(docs) == 0:
+            empty_set = {}
+            del query_word_ids[-1]
+            if len(query_word_ids) == 0:
+                break
+            docs = search_inverted(empty_set, query_word_ids, len(query_word_ids) - 1)
         if len(docs) == 0:
-            print("No results")
+            print('No results')
         # Calculate rank
         else:
             rank = {}
             for doc_id in docs:
                 rank[doc_id] = 0
                 for word_id in query_word_ids:
-                    if inverted_index[str(word_id)][doc_id][0] == -1: # if fancy hit
+                    hit = inverted_index[str(word_id)][doc_id]
+                    hit = int(bin(hit & 65535), 2) # to check if MSB is 1 for 16 bit num
+                    if hit >= 32768: # if MSB == 1 i.e it is a fancy hit
                         rank[doc_id] += 50 # rank increment for fancy hit
-                        rank[doc_id] += inverted_index[str(word_id)][doc_id][1]
+
+                    hit = int(bin(hit & 32767), 2) # 32767 = 0111_1111_1111_1111
+                    rank[doc_id] += hit # add the remaining number after setting MSB to 0
                     
-                    rank[doc_id] += inverted_index[str(word_id)][doc_id][0]
 
             rank = sorted(rank.items(), key=lambda x: x[1], reverse=True) # return list of tuples
-
+            #print(rank)
             for doc_id, rank_n in rank:
                 print(doc_index[doc_id])
 
---
+++ update_forward_index.py
@@ -0,0 +1,88 @@
+import json
+from pathlib import Path
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+paths = Path("newsdata")
+all_articles_paths = [p for p in paths.glob('*.json')]
+
+# load lexicon, document index and forward index into memory
+lexicon = json.loads(Path('lexicon.json').read_text())
+doc_index = json.loads(Path('doc_index.json').read_text())
+forward_index = json.loads(Path('forward_index.json').read_text())
+
+# get the next doc_id and word_id from doc_index and lexicon
+next_doc_id = int(max(doc_index.keys()))
+next_doc_id += 1
+next_word_id = max(lexicon.values())
+next_word_id += 1
+
+# construct a stemmer and get stopwords
+stemmer = SnowballStemmer('english')
+stop_words = set(stopwords.words('english'))
+
+# get previous document urls
+prev_docs = doc_index.values()
+
+print("Updating forward index...")
+# for every file in newsdata/ folder
+for articles_path in all_articles_paths:
+    articles = json.loads(Path(articles_path).read_text())
+    # for every article in one file
+    for article in articles:
+        # If article's url not in prev_docs, then we
+        # update our lexicon, doc_index and forward index
+        if article['url'] not in prev_docs:
+            hit_list = {}
+            doc_index[next_doc_id] = article['url']
+            text = article['content']
+            # split into words
+            tokens = word_tokenize(text)
+            # convert to lower case
+            tokens = [w.lower() for w in tokens]
+            # remove punctuation from each word
+            table = str.maketrans('', '', string.punctuation)
+            stripped = [w.translate(table) for w in tokens]
+            # remove remaining tokens that are not alphabetic
+            words = [word for word in stripped if word.isalpha()]
+            # remove duplicates
+            # filter out stop words
+            words = [w for w in words if not w in stop_words]
+            # stem the words
+            words = [stemmer.stem(w) for w in words]
+            for word in words:
+                # insert word in lexicon that is not already in lexicon
+                if word not in lexicon:
+                    lexicon[word] = next_word_id
+                    next_word_id += 1
+
+            # make hit list
+            for word in words:
+                temp_hit = 0
+                if lexicon[word] not in hit_list:
+                    if (word in article['title']) or word in article['url']: # if fancy hit
+                        # we set the MSB to one in hit to indicate that the hit is fancy
+                        temp_hit = int(bin(temp_hit | 32768), 2) # 32768 == 0b1000_0000_0000_0000
+                    
+                    temp_hit += 1
+                    hit_list[lexicon[word]] = temp_hit
+
+                else:
+                    hit_list[lexicon[word]] += 1 # increment the occurances already in hit list
+            
+
+            forward_index[next_doc_id] = hit_list # insert hit list in forward index
+            next_doc_id += 1 # get the next doc_id
+
+
+# write lexicon to file
+lex_data = json.dumps(lexicon)
+Path('lexicon.json').write_text(lex_data)
+# write doc_index to file
+doc_index_data = json.dumps(doc_index)
+Path('doc_index.json').write_text(doc_index_data)
+# write forward_index to file
+forward_index_data = json.dumps(forward_index)
+Path('forward_index.json').write_text(forward_index_data)
